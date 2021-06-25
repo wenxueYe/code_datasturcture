748cb99530fd046bfff249fcffb4acf301b223d2
wenxueYe <yewenxuewy@163.com>
2021-06-25 09:27:51 +0800

    append a few char opt

--- arraytest.c
+++ arraytest.c
@@ -8,5 +8,24 @@ int main() {
 	for(i=0;i<5;i++) {
 		printf("%d\n",*(*a+i));
 	}
+	printf("======\n");
+	//static char s[5];//NULL
+//	char s[5] = {'a','b','c'};
+//	s[3] = 'f';
+//	s[4] = 'e';
+//	s[4] = '\0';
+//	char s[5] = "";
+//	for(i=0;i<5;i++) {
+//		printf("%c\n",*(s+i));
+//	}
+	
+	
+	printf("======\n");
+	i = 0;
+	char s2[] = "ABCD";
+	while(*(s2+i)) {
+		printf("%c\n",*(s2+i));
+		i++;
+	}
 	return 0;
 }
